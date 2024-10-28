29a36a0b45e4d942b6b23e3a621fb444dae189c1
Glender Miranda <glender.miranda@epn.edu.ec>
2024-10-27 20:39:50 -0500

    feat: added the first components to tests

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+import React from "react";
+import {
+  DigitalClock,
+  Header,
+  ImageGrid,
+  StyledButton,
+} from "glenderm04-my-first-component-library";
+import { Navbar } from "glenderm04-my-first-component-library";
+
+
+const navItems = [
+  { label: "Inicio", link: "/" },
+  { label: "Servicios", link: "/servicios" },
+  { label: "Contacto", link: "/contacto" },
+];
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Header />
+      <Navbar items={navItems} />
+      <h1>Home Page</h1>
+      <StyledButton route={"/home"}>Click me</StyledButton>
+      <DigitalClock />
+      <ImageGrid />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
